4cb4ed01e77edc8e21c6794d4d10b196728c1349
ustjavafsdb222 <235794@ust.com>
2022-10-31 17:46:48 +0530

    Changed

--- JAVA/day3/Animal.java
+++ JAVA/day3/Animal.java
@@ -1,23 +1,43 @@
 package day3;
 
+/**
+ * The type Animal.
+ */
 public class Animal {
 
+	/**
+	 * Display.
+	 */
 	public void display() {
 		System.out.println("I am an animal");
 	}
 }
 
+/**
+ * The type Dog.
+ */
 class Dog extends Animal{
 	public void display() {
 		System.out.println(" i am a dog");
 	}
-	
+
+	/**
+	 * Print message.
+	 */
 	public void printMessage() {
 		display();
 	}
 }
 
+/**
+ * The type Main 6.
+ */
 class Main6{
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 	public static void main (String args[]) {
 		Dog dog1 = new Dog();
 		dog1.printMessage();
